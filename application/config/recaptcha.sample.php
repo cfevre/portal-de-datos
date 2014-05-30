a18c30efa37c730c3afdd739c18d790906612dc5
cidro <pablo.canaleshernandez@gmail.com>
2014-05-30 11:25:22 -0400

    Se limpia clave de recaptcha

--- application/config/recaptcha.sample.php
+++ application/config/recaptcha.sample.php
@@ -1,4 +1,4 @@
 <?php
-$config['recaptcha_pubkey'] = '6LdPI9kSAAAAADIKGtUlaNYYcH55_4eviM0zwxc3';
-$config['recaptcha_prikey'] = '6LdPI9kSAAAAAIWfnAdKQsMMWJWJ-E2rHVrMayxz';
+$config['recaptcha_pubkey'] = '';
+$config['recaptcha_prikey'] = '';
 ?>
