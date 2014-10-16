4fce212f813dcd1ae687f1bee3e5206f0a3fb946
mbalmaceda <mbalmaceda.17@gmail.com>
2014-10-16 11:46:21 -0300

    fix #83, fix #84, fix #85

--- application/views/dataset/catalogo.php
+++ application/views/dataset/catalogo.php
@@ -2,12 +2,8 @@
 	<h2 class="catalogo">Catálogo de Datos <small>&nbsp;Datasets publicados: <strong><?php echo number_format($ndatasets,0,',','.'); ?></strong></small></h2>
 	<div>
 		<?php echo widgetHelper::catalogosPorEntidad(); ?>
-		<?php echo widgetHelper::catalogosTop(false); ?>
 	</div>
 </div>
 <div class="span4 side-bar">
-    <?php echo widgetHelper::categoriasConMasDatasets(); ?>
-    <?php echo widgetHelper::etiquetasPopulares(); ?>
-	<?php echo widgetHelper::banner('colabora'); ?>
-    <?php echo widgetHelper::banner('gobiernoabierto'); ?>   
+    <?php echo widgetHelper::categoriasConMasDatasets(); ?> 
 </div>
