13e39ea9d006aeebfff0645cda1c4e5ce1926373
Pablo H. Paladino <paladinop@gmail.com>
2014-10-23 10:46:16 -0300

    abrecl redirect

--- application/controllers/abrecl.php
+++ application/controllers/abrecl.php
@@ -1,13 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Visualizaton extends CIE_Controller {
+class AbreCL extends CIE_Controller {
 
 	public function __construct(){
 		parent::__construct();
 	}
 
   public function index() {
-  	redirect('http://visualizaton.datos.gob.cl');
+  	redirect('http://abrecl.datos.gob.cl');
   }
 
 }
