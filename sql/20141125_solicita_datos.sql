3ec66e879d8128e52798fc06bf980e32367ad7e1
mbalmaceda <mbalmaceda.17@gmail.com>
2014-11-25 15:22:55 -0300

    fix sql_solicitud_datos

--- sql/20141125_solicita_datos.sql
+++ sql/20141125_solicita_datos.sql
@@ -4,7 +4,7 @@ ALTER TABLE `participacion` ADD `ocupacion` VARCHAR(255) CHARACTER SET utf8 COLL
 ALTER TABLE `participacion` ADD `institucion` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL AFTER `mensaje`;
 ALTER TABLE `participacion` CHANGE `categoria` `categoria` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 ALTER TABLE `participacion` ADD `enlace` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ;
-ALTER TABLE `participacion` ADD `votacion` INT NULL AFTER;
+ALTER TABLE `participacion` ADD `votacion` INT NULL;
 ALTER TABLE users 
 ADD CONSTRAINT `fk_users_servicio1` 
 FOREIGN KEY (`servicio_codigo`) 
